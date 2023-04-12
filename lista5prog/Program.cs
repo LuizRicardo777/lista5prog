ff1c59edb3305a80477e3179abd73deb050f5859
LuizRicardo777 <l.salles@aluno.ifsp.edu.br>
2023-04-11 21:45:05 -0300

    final

--- lista5prog/Program.cs
+++ lista5prog/Program.cs
@@ -37,7 +37,7 @@ namespace lista5prog
                     Console.ReadKey();
                     break;
 
-                    case 2:
+                case 2:
                     int n1;
                     Console.WriteLine("digite um numero");
                     n1 = int.Parse(Console.ReadLine());
@@ -80,7 +80,7 @@ namespace lista5prog
                     break;
 
                 case 4:
-                      double soma = 0 ,num;
+                    double soma = 0, num;
                     for (; soma <= 200;)
                     {
                         Console.WriteLine("entre com um numero");
@@ -90,7 +90,7 @@ namespace lista5prog
                         {
                             soma += num;
                             Console.WriteLine("numero positivo " + num +
-                                              " valor da soma atual " + soma );
+                                              " valor da soma atual " + soma);
                         }
                         else
                         {
@@ -104,35 +104,85 @@ namespace lista5prog
 
                 case 5:
 
+                    double nu;
 
+                    Console.Write("Digite um número inteiro: ");
+                    nu = double.Parse(Console.ReadLine());
 
+                    Console.WriteLine("Divisores de " + nu + ":");
 
+                    for (int i = 1; i <= nu; i++)
+                    {
+                        if (nu % i == 0)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        else
+                        {
+                            Console.WriteLine("este numero não é esperado, tente um inteiro");
+                        }
+                    }
 
                     break;
+
+                case 6:
+
+                    int maior = int.MinValue;
+                    int menor = int.MaxValue;
+
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        Console.Write("Digite o " + i + "º número: ");
+                        int nm = int.Parse(Console.ReadLine());
+
+                        if (nm > maior)
+                        {
+                            maior = nm;
+                        }
+
+                        if (nm < menor)
+                        {
+                            menor = nm;
+                        }
+                    }
+
+            
+                        
+                    
+            
+                    Console.WriteLine("O maior número digitado foi: " + maior);
+                    Console.WriteLine("O menor número digitado foi: " + menor);
+                    Console.ReadKey();
+
+                    break;
+
+                    case 7:
+
+                    int numero9, somaPares = 0, somaImpares = 0;
+
+                    for (Console.Write("Digite um número (0 para sair): "), numero9= int.Parse(Console.ReadLine()); numero9 != 0; Console.Write("Digite um número (0 para sair): "), numero9 = int.Parse(Console.ReadLine()))
+                    {
+                        if (numero9 % 2 == 0)
+                        {
+                            somaPares += numero9;
+                        }
+                        else
+                        {
+                            somaImpares += numero9;
+                        }
+                    }
+
+                    Console.WriteLine("Soma dos números pares: " + somaPares);
+                    Console.WriteLine("Soma dos números ímpares: " + somaImpares);
+                    Console.ReadKey();
+                    break;
+
+
 
                 default:
+                    Console.WriteLine("esta opção não existe");
                     break;
-
-            }
-
-
-
-
-           
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+           }
 
 
 
